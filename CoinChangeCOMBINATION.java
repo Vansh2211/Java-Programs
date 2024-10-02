46a5e7ddb205a5657ef33a77426977bade1cff2d
Vansh Mehta <mehtavansh2211@gmail.com>
2024-10-02 11:41:13 +0530

    Dynamic Programming

--- CoinChangeCOMBINATION.java
+++ CoinChangeCOMBINATION.java
@@ -7,15 +7,19 @@ public class CoinChangeCOMBINATION {
         // eg. {2,3,5} and amount = 7 
         // so combinations are - 223,25
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array");
         int n = sc.nextInt();
        
 
         int arr[] = new int[n];
+        System.out.println("Enter array elements");
         for(int i=0;i<n;i++){
             arr[i] = sc.nextInt();
         }
 
+        System.out.println("Enter amount");
         int amount = sc.nextInt();
+
         int dp[] = new int[amount+1];
         dp[0] = 1;
 
