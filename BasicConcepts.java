e2f7fb473744b134fb8e1ddcb88d63d328eb72d6
Vansh Mehta <mehtavansh2211@gmail.com>
2024-10-07 13:46:48 +0530

    Basic

---
+++ BasicConcepts.java
@@ -0,0 +1,34 @@
+// import java.util.Scanner;
+
+public class BasicConcepts {
+    public static void main(String[] args) {
+        
+    for(int i = 0; i < 5; i++) { 
+
+        System.out.print(i + ' '); 
+       
+    
+    } 
+
+}
+}
+
+//     public static void main(String[] args) {    
+//     String nn = "Vansh";
+//     Scanner sc = new Scanner(System.in);
+//     System.out.println("Enter size of array");
+//     int n = sc.nextInt();
+
+
+//     // boolean flag = true;
+//     // if(flag){
+//     // for(int i=0;i<n.length()-1;i++){
+//     //     System.out.println(i + "_");
+//     // }
+//     // }
+//     // else{
+//     //     System.out.println("NoName");
+//     }
+
+
+// }
