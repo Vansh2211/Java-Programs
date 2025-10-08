9c7e125424a91b2a6a626639ebc5fbf78a689102
Vansh2211 <168256766+Vansh2211@users.noreply.github.com>
2025-10-08 12:31:36 +0530

    Leetcode 516

--- LongestPalindromeSubsequence.java
+++ LongestPalindromeSubsequence.java
@@ -3,7 +3,8 @@ class Solution {
         String rev = new StringBuilder(s).reverse().toString();
         int n = s.length();
         int dp[][] = new int[n+1][n+1];
-
+// we are using very easy method here
+        // we can also use LCS like this : LPS = LCS + rev(LCS)
 
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
