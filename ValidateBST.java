e895382da3e265432f6ed4fec040f13bf31441ea
Vansh Mehta <mehtavansh2211@gmail.com>
2025-11-13 12:22:50 +0530

    some changes

--- ValidateBST.java
+++ ValidateBST.java
@@ -1,4 +1,17 @@
-class Solution {
+import java.util.*;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class ValidateBST {
     public boolean isValidBST(TreeNode root) {
         List<Integer> inorder = new ArrayList<>();
         inorderTraversal(root, inorder);
